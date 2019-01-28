3ff8cb0609afe9a30c23e884efbd106f068c6fac
pragadeeshrock <46838550+pragadeeshrock@users.noreply.github.com>
2019-01-29 00:03:21 +0530

    Create armss.c

---
+++ armss.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+int temp,temp1,a,b,i,c=0,rem,sum=0;
+clrscr();
+scanf("%d%d",&a,&b);
+for(i=a+1;i<b;i++)
+{
+temp=i;
+temp1=i;
+sum=0;
+c=0;
+while(i>0)
+{
+i=i/10;
+c++;
+}
+while(temp>0)
+{
+rem=temp%10;
+sum=sum+pow(rem,c);
+temp=temp/10;
+}
+if(sum==temp1)
+{
+printf("%d",sum);
+}
+}
+getch();
+}
