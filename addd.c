951054de48596809af1e56a0af2efeb157719bcf
pragadeeshrock <46838550+pragadeeshrock@users.noreply.github.com>
2019-01-28 22:14:34 +0530

    Create addd.c

---
+++ addd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[100],b=0,i,n,k;
+clrscr();
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+scanf("%d",&K);
+for(i=0;i<k;i++)
+{
+b=b+a[i];
+}
+printf("%d",b);
+getch();
+}
