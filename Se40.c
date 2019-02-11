95dd1df5008eff1bfa3da01023bce5c37d208a71
pragadeeshrock <46838550+pragadeeshrock@users.noreply.github.com>
2019-02-11 13:52:09 +0530

    Create Se40.c

---
+++ Se40.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) {
+int i, n, t1 = 0, t2 = 1, n1;
+scanf("%d", &n);
+for (i = 1; i <= n; ++i)
+    {
+        n1 = t1 + t2;
+        t1 = t2;
+        t2 = n1;
+        printf("%d ", t1);
+    }
+return 0;
+}
