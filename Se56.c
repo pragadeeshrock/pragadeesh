609c54a9376170ac314c0f065b611a0964862c39
pragadeeshrock <46838550+pragadeeshrock@users.noreply.github.com>
2019-02-11 14:14:18 +0530

    Create Se56.c

---
+++ Se56.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n<=10000000)
+    {
+    n++;
+    break;
+    }
+    printf("%d",n);
+    
+
+    return 0;
+}
