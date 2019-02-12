66cd4907009bcc9fd1ca65e4ea5dda9638db63d0
pragadeeshrock <46838550+pragadeeshrock@users.noreply.github.com>
2019-02-12 06:57:20 +0530

    Create Se93.c

---
+++ Se93.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,k,i,a[10];
+	scanf("%d",&n);
+	scanf("%d",&k);
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	printf("%d",a[k-1]);
+	return 0;
+}
