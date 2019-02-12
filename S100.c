6c14016f5ad9c113c9e85a360416a00e9c995278
pragadeeshrock <46838550+pragadeeshrock@users.noreply.github.com>
2019-02-12 21:40:36 +0530

    Create S100.c

---
+++ S100.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	int l,b,h,tsa,volume;
+	scanf("%d %d %d",&l,&b,&h);
+	volume=l*b*h;
+	tsa=2*((l*b)+(b*h)+(h*l));
+	printf("volume=%d\n",volume);
+	printf("TSA=%d",tsa);
+	return 0;
+}
