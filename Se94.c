38ceaae289bf5670496c3802024ec8fa4fb059ed
pragadeeshrock <46838550+pragadeeshrock@users.noreply.github.com>
2019-02-12 07:00:45 +0530

    Create Se94.c

---
+++ Se94.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+	float p,r,si;
+	int n,result;
+	scanf("%f %d %f",&p,&n,&r);
+	si=((p*n*r)/100);
+	result=floor(si);
+	printf("%d",result);
+	return 0;
+}
