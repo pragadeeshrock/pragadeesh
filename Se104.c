cf93a604a10225126367832b705f50f51565c3dd
pragadeeshrock <46838550+pragadeeshrock@users.noreply.github.com>
2019-02-12 07:27:39 +0530

    Create Se104.c

---
+++ Se104.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include@<conio.h>
+int main()
+{
+    int n,k,res=1,i;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<k;i++)
+    {
+        res=res*n;
+    }
+    printf("%d",res);
+	return 0;
+}
