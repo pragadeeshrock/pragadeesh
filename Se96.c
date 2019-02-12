711edcef2de2840edb32a5f683fe3d362a471289
pragadeeshrock <46838550+pragadeeshrock@users.noreply.github.com>
2019-02-12 07:05:47 +0530

    Create Se96.c

---
+++ Se96.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,rev=0,rem=0;
+    scanf("%d",&n);
+    while(n!='\0')
+    {
+        rem=n%10;
+        rev=(rev*10)+rem;
+        n=n/10;
+    }
+    printf("%d",rev);
+}
