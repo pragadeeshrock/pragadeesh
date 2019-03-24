a2c38ae8d8e96db696f7a3828bcef4f02961ec3f
pragadeeshrock <46838550+pragadeeshrock@users.noreply.github.com>
2019-03-24 19:28:16 +0530

    Create P116.c

---
+++ P116.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+
+void main()
+{
+int n,a[100],b[100],i,j,k=0,c=0,t;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+    if(a[i]!='*')
+    {
+c=0;
+for(j=i+1;j<n;j++)
+{
+    if(a[j]!='*')
+    {
+if(a[i]==a[j])
+{
+c++;
+a[j]='*';
+}
+}
+}
+if(c>0)
+{
+b[k]=a[i];
+k++;
+a[i]='*';
+}
+}
+}
+b[k]=NULL;
+for(i=0;i<k;i++)
+{
+for(j=i+1;j<k;j++)
+{
+    if(b[i]<b[j])
+    {
+        t=b[j];
+        b[j]=b[i];
+        b[i]=t;
+    }
+}
+}
+for(i=0;a[i]!=NULL;i++)
+{
+for(j=i+1;a[j]!=NULL;j++)
+{
+    if(a[i]<a[j])
+    {
+        t=a[j];
+        a[j]=a[i];
+        a[i]=t;
+    }
+}
+}
+for(i=0;i<k;i++)
+{
+    printf("%d ",b[i]);
+}
+for(i=0;i<n;i++)
+{
+    if(a[i]!='*')
+    {
+    printf("%d ",a[i]);
+    }
+}
+}
